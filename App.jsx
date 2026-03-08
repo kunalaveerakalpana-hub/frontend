2c11843414239e9fd3dd2a14ef72407257050e12
kunalaveerakalpana-hub <kunalaveerakalpana@gmail.com>
2026-03-08 13:16:58 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route}from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import InstructorDashboard from './pages/InstructorDashboard';
 import CreateCoursePage from './pages/CreateCoursePage';
 import AdminCoursesPage from './pages/AdminCoursesPage';
-
-function App() {
-  return (
+function App(){
+  return(
     <Router>
       <Routes>
-        {/* Member 8 Routes */}
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-
-        {/* Member 10 Routes */}
-        <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
-        <Route path="/create-course" element={<CreateCoursePage />} />
-        <Route path="/admin" element={<AdminCoursesPage />} />
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/register" element={<RegistrationPage/>}/>
+        <Route path="/instructor-dashboard" element={<InstructorDashboard/>}/>
+        <Route path="/create-course" element={<CreateCoursePage/>}/>
+        <Route path="/admin" element={<AdminCoursesPage/>}/>
       </Routes>
     </Router>
   );
 }
-
 export default App;
