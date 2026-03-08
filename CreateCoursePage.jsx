28fef5f80a8f094db6dc0f9f07a05f77e7a24417
kunalaveerakalpana-hub <kunalaveerakalpana@gmail.com>
2026-03-08 13:23:44 +0530

    Update CreateCoursePage.jsx

--- CreateCoursePage.jsx
+++ CreateCoursePage.jsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-function CreateCoursePage() {
-    const [course, setCourse] = useState({ title: "", description: "", category: "FSD" });
-    const navigate = useNavigate();
-
-    function handleCreate() {
-        const allCourses = JSON.parse(localStorage.getItem("allCourses")) || [];
-        const newCourse = {
+import React,{useState}from "react";
+import {useNavigate}from "react-router-dom";
+function CreateCoursePage(){
+    const [course,setCourse]=useState({title:"",description:"",category:"FSD" });
+    const navigate=useNavigate();
+    function handleCreate(){
+        const allCourses=JSON.parse(localStorage.getItem("allCourses"))||[];
+        const newCourse={
             ...course,
-            id: Date.now(),
-            status: "PENDING", // FR-01: Starts as pending
-            instructor: "Current Instructor",
-            lessons: []
+            id:Date.now(),
+            status:"PENDING", 
+            instructor:"Current Instructor",
+            lessons:[]
         };
         allCourses.push(newCourse);
-        localStorage.setItem("allCourses", JSON.stringify(allCourses));
+        localStorage.setItem("allCourses",JSON.stringify(allCourses));
         alert("Course created and sent for Admin approval!");
         navigate("/instructor-dashboard");
     }
-
-    return (
+    return(
         <div>
             <h2>Create New Course</h2>
             <input type="text" placeholder="Course Title" onChange={(e) => setCourse({...course, title: e.target.value})} /><br/>
