278e654b66cc0134fe413fdf2fc6c549911b8c78
kunalaveerakalpana-hub <kunalaveerakalpana@gmail.com>
2026-03-08 13:29:07 +0530

    Update LoginPage.jsx

--- LoginPage.jsx
+++ LoginPage.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useState}from "react";
+import {useNavigate}from "react-router-dom";
 import AuthService from "../services/AuthService";
 import './styles.css';
-
-function LoginPage() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [role, setRole] = useState("STUDENT");
-
-    function handleLogin() {
-        const user = AuthService.login(email, password);
-        if (user) {
-            // Redirect based on role if necessary, or to a general dashboard
+function LoginPage(){
+    const navigate=useNavigate();
+    const [email,setEmail]=useState("");
+    const [password, setPassword]=useState("");
+    const [role, setRole]=useState("STUDENT");
+    function handleLogin(){
+        const user=AuthService.login(email,password);
+        if (user){
             navigate("/courses");
-        } else {
+        } 
+        else{
             alert("Invalid email or password. Please register first.");
         }
     }
-
-    return (
+    return(
         <div>
             <h2>Login Page</h2>
-            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} /><br />
-            <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} /><br />
-            <label>Login as: </label>
-            <select value={role} onChange={(e) => setRole(e.target.value)}>
+            <input type="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/><br/>
+            <input type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/><br/>
+            <label>Login as:</label>
+            <select value={role} onChange={(e)=>setRole(e.target.value)}>
                 <option value="STUDENT">Student</option>
                 <option value="INSTRUCTOR">Instructor</option>
                 <option value="ADMIN">Admin</option>
@@ -34,5 +31,4 @@ function LoginPage() {
         </div>
     );
 }
-
 export default LoginPage;
